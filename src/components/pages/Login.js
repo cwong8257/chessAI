ba616b5e21bc65187eb88481bdce1987c73d8fcb
Christopher Wong <cwong8257@gmail.com>
2019-05-02 23:46:07 -0400

    Adds login form and updates package.json

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -7,16 +7,39 @@ class Login extends Component {
     password: '',
   };
 
-  onChange = (e) => {
+  handleOnChange = (e) => {
     const { name, value } = e.target;
     this.setState(() => ({ [name]: value }));
   };
+
+  handleSubmitForm = (e) => {
+    e.preventDefault();
+
+    const { email, password } = this.state;
+    console.log(`signed in with ${email} and ${password}`);
+  }
 
   render() {
     const { email, password } = this.state;
 
     return (
-      <div>Login</div>
+      <div>
+        <form onSubmit={this.handleSubmitForm}>
+          <div className="form-group">
+            <label htmlFor="email">
+              Email address
+              <input type="email" className="form-control" id="email" name="email" value={email} onChange={this.handleOnChange} />
+            </label>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">
+              Password
+              <input type="password" className="form-control" id="password" name="password" value={password} onChange={this.handleOnChange} />
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary">Sign in</button>
+        </form>
+      </div>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
     },
   },
   plugins: [
-    new CleanWebpackPlugin([outputDirectory]),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './public/index.html',
       favicon: './public/favicon.ico',
