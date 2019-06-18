3efd3aa847a91bf91b450bac3049e3e7e1773db2
Christopher Wong <cwong8257@gmail.com>
2019-06-17 22:55:17 -0400

    Add options on Home page to play Human or Machine

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,9 +3,31 @@ import React from 'react';
 class Home extends React.Component {
   state = {}
 
+  handleOnClickHuman = (e) => {
+    // call endpoint for playing Human
+    // route to game
+  }
+
+  handleOnClickMachine = (e) => {
+    // call endpoint for playing Machine
+    // route to game
+  }
+
   render() {
     return (
-      <div>Home</div>
+      <div className="home">
+        <div className="row">
+          <div className="col-12 mb-3">
+            <h2 className="text-center">Play</h2>
+          </div>
+          <div className="col-12 col-sm-6 text-center">
+            <button type="button" className="btn btn-primary btn-lg btn-block" onClick={this.handleOnClickHuman}>Human</button>
+          </div>
+          <div className="col-12 col-sm-6 text-center">
+            <button type="button" className="btn btn-primary btn-lg btn-block" onClick={this.handleOnClickMachine}>Machine</button>
+          </div>
+        </div>
+      </div>
     );
   }
 }
